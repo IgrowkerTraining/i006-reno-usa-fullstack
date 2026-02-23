32d5e07867c84a63bfd9e912a6e33dd2a3712b58
Melina Bueno <melinabueno123@gmail.com>
2026-02-23 20:18:05 -0300

    create-task-model

---
+++ apps/backend/src/models/Task.js
@@ -0,0 +1,33 @@
+class Task {
+    constructor({
+        id,
+        name,
+        description,
+        phaseId,
+        tradeId,
+    }) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.phaseId = phaseId;
+        this.tradeId = tradeId;
+    }
+
+    toJSON() {
+        return {
+            id: this.id,
+            name: this.name,
+            description: this.description,
+            phaseId: this.phaseId,
+            tradeId: this.tradeId,
+        };
+    }
+
+    static create(taskData) {
+        return new Task({
+            ...taskData,
+        });
+    }
+}
+
+module.exports = Task;
