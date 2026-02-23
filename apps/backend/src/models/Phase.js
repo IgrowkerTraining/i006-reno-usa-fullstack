3f9b6ad61a93205a65b0ab1f878c89cc0d3d2953
Melina Bueno <melinabueno123@gmail.com>
2026-02-23 20:08:59 -0300

    create-phase-model

---
+++ apps/backend/src/models/Phase.js
@@ -0,0 +1,33 @@
+class Phase {
+    constructor({
+        id,
+        name,
+        planned_start,
+        planned_end,
+        projectId, 
+    }) {
+        this.id = id; 
+        this.name = name;
+        this.planned_start = planned_start; 
+        this.planned_end = planned_end; 
+        this.projectId = projectId;
+    }
+
+    toJSON() {
+        return {
+            id: this.id,
+            name: this.name,
+            planned_start: this.planned_start,
+            planned_end: this.planned_end,
+            projectId: this.projectId,
+        };
+    }
+
+    static create(phaseData) {
+        return new Phase({
+            ...phaseData,
+        });
+    }
+}
+
+module.exports = Phase;
