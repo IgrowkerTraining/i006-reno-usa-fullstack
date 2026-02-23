3178aa91b26edb79e6b01d9bf0099ad0fcd1bb9b
Melina Bueno <melinabueno123@gmail.com>
2026-02-23 20:22:52 -0300

    create-trade-model

---
+++ apps/backend/src/models/Trade.js
@@ -0,0 +1,27 @@
+class Trade {
+    constructor({
+        id,
+        name,
+        description,
+    }) {
+        this.id = id; 
+        this.name = name; 
+        this.description = description;
+    }
+
+    toJSON() {
+        return {
+            id: this.id,
+            name: this.name,
+            description: this.description,
+        };
+    }
+
+    static create(tradeData) {
+        return new Trade({
+            ...tradeData,
+        });
+    }
+}
+
+module.exports = Trade;
