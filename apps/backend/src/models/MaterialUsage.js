b66c9a7ef6473d783a497e7334cd6c5400d5871c
Melina Bueno <melinabueno123@gmail.com>
2026-02-23 20:44:41 -0300

    create-material-usage-model

---
+++ apps/backend/src/models/MaterialUsage.js
@@ -0,0 +1,30 @@
+class MaterialUsage {
+    constructor({
+        id,
+        quantity,
+        executedTaskId,
+        materialId,
+    }) {
+        this.id = id;
+        this.quantity = quantity;
+        this.executedTaskId = executedTaskId;
+        this.materialId = materialId;
+    }
+
+    toJSON() {
+        return {
+            id: this.id,
+            quantity: this.quantity,
+            executedTaskId: this.executedTaskId,
+            materialId: this.materialId,
+        };
+    }
+
+    static create(data) {
+        return new MaterialUsage({
+            ...data,
+        });
+    }
+}
+
+module.exports = MaterialUsage;
