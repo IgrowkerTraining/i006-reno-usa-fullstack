2bb554c955a2190db8af3a5ea77ded442699e1a8
Carlos Enrique Contreras Duque <carlos.contredu@gmail.com>
2026-03-05 18:07:00 +0000

    errores añadidos

--- apps/frontend/src/pages/Dashboard.tsx
+++ apps/frontend/src/pages/Dashboard.tsx
@@ -1,97 +1,139 @@
 import React, { useState, useEffect } from "react";
-import { User } from "../types";
-import { Button } from "../components/common/Button";
+import { User, Project } from "../types";
 import { getAIGreeting } from "../services/service";
 import { api } from "../services/authServices";
 import { useAuth } from "../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
+import { projectService } from "../services/project.service"
+import error_icons from "../components/common/error_icons";
+
+
 const Dashboard: React.FC = () => {
 
-  // Prototipo de proyectos activos
-  const projects = [
-    { id: "cmm5x88kw000bup5loxgrm4f2", name: "Project Alpha", status: "In Progress" },
-    { id: 2, name: "Project Beta", status: "Completed" },
-    { id: 3, name: "Project Gamma", status: "In Progress" },
-    { id: 4, name: "Project Delta", status: "In Progress" },
-    { id: 5, name: "Project Epsilon", status: "Completed" },
-  ];
-
+  const { user } = useAuth();
 
   const navigate = useNavigate();
 
-  // const [greeting, setGreeting] = useState("");
-  // const [isBackendOnline, setIsBackendOnline] = useState(false);
+  // const [projects, setProjects] = useState<Project[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [greeting, setGreeting] = useState("");
 
+  const projects = [
+    { id: "1", name: "project alpha", status: "progress" }
+  ]
+
+  const error = [
+    { id: "1", name: "ERROR-01", icon: error_icons["ERROR-01"] },
+    { id: "2", name: "ERROR-02", icon: error_icons["ERROR-02"] },
+    { id: "3", name: "ERROR-03", icon: error_icons["ERROR-03"] }
+
+  ]
   // useEffect(() => {
-  //   const initDashboard = async () => {
-  //     const [msg, online] = await Promise.all([
-  //       getAIGreeting(user?.name || ""),
-  //       api.checkHealth(),
-  //     ]);
-  //     setGreeting(msg);
-  //     setIsBackendOnline(online);
-  //   };
-  //   initDashboard();
-  // }, [user?.name]);
+
+  //   if (!user) {
+  //     navigate("/login")
+  //     return;
+  //   }
+
+  //   const fetchProjects = async () => {
+  //     try {
+  //       const data = await projectService.getAll()
+  //       setProjects(data)
+
+  //       const msg = await getAIGreeting(user.name)
+  //       setGreeting(msg);
+
+  //     } catch (error) {
+  //       console.log(error)
+  //     } finally {
+  //       setLoading(false)
+  //     }
+  //   }
+
+  //   fetchProjects();
+  // }, [])
+
+  // if (loading) return (<div className="flex flex-col items-center justify-center h-screen bg-white gap-4">
+  //   <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-green-600"></div>
+  //   <p className="text-gray-700 font-bold text-lg">Loading projects...</p>
+  // </div>
+  // )
 
   return (
+    <>
+      <div className="bg-white h-screen">
+        {/* {greeting && (
+          <p className="text-2xl font-bold text-black mb-4">{greeting}</p>
+        )} */}
 
-    <div>
-      {projects.length === 0 ? (
-        <div className="text-center">
-          <p className="text-xl font-extrabold mt-10">There are no active projects</p>
-          <div className="p-2 text-center rounded-md bg-blue-900 mt-4">
-            <button type="submit" className="text-white text-xl font-mono" onClick={() => navigate("/Vista-proyecto")}>
-              New Project
-            </button>
+        {projects.length === 0 ? (
+          <div className="text-center text-black bg-white">
+            <p className="text-xl font-extrabold pt-10">There are no active projects</p>
+            <div className="p-2 mx-4 text-center rounded-md bg-blue-900 mt-10">
+              <button type="submit" className="text-white text-xl font-mono hover:bg-blue-600 hover:scale-95 transition-transform" onClick={() => navigate("#")}>
+                New Project
+              </button>
+            </div>
           </div>
-        </div>
-      ) : (
-        <div className="container text-black mx-auto mt-10 p-4">
-          <h1 className="text-xl font-extrabold font-serif text-blue-950">
-            ALLOCATED PROJECTS
-          </h1>
+        ) : (
+          <div className="text-black mx-auto pt-10 p-4">
+            <h1 className="text-xl font-extrabold font-serif text-blue-950">
+              ALLOCATED PROJECTS
+            </h1>
 
-          <div className="my-4 space-y-4">
-            {projects.map((project) => (
-              <div
-                key={project.id}
-                className="flex p-4 gap-5 items-center rounded-lg bg-sky-100"
-              >
-                <div className="flex-auto">
-                  <h2 className="text-xl font-semibold border-b border-gray-500">
-                    {project.name}
-                  </h2>
-
-                  <div className="flex gap-2 my-1">
-                    <p>{project.status}</p>
-
-                    <section className="flex gap-2 text-center">
-                      <p className="size-6 rounded-full bg-gray-400"></p>
-                      <p className="size-6 rounded-full bg-gray-400"></p>
-                      <p className="size-6 rounded-full bg-gray-400"></p>
-                    </section>
-                  </div>
-                </div>
-
-                <button
-                  type="button"
-                  className="rounded w-35 p-2 bg-blue-900 text-white"
-                  onClick={() => navigate(`/proyecto/${project.id}`)}
+            <div className="mt-4 space-y-4">
+              {projects.map((project) => (
+                <div
+                  key={project.id}
+                  className="flex p-4 gap-5 items-center rounded-lg bg-sky-100"
                 >
-                  Edit Project
-                </button>
-              </div>
-            ))}
+                  <div className="flex-auto">
+                    <h2 className="text-xl font-semibold border-b border-gray-500">
+                      {project.name}
+                    </h2>
+
+                    <div className="flex gap-2 my-1 items-center">
+                      <p>{project.status}</p>
+
+                      {error.length === 0 ? (
+                        <p className="size-6 rounded-full bg-gray-400">There are not errors</p>) :
+                        (
+                          error.map((err) => {
+                            const errorInfo = error_icons.find((em) => em.error_code === err.name
+                            );
+                            return (
+                              <div
+                                key={err.id}
+                                className={`flex items-center justify-center px-2 py-2 rounded-lg text-white font-medium
+                                            ${errorInfo?.color_assigned}`}
+                              >
+                                {errorInfo?.icon}
+                              </div>
+                            )
+                          })
+                        )}
+                    </div>
+                  </div>
+
+                  <button
+                    type="button"
+                    className="rounded w-35 p-2 bg-blue-900 text-white hover:bg-blue-600 hover:scale-95 transition-transform"
+                    onClick={() => navigate(`/proyecto/${project.id}`)}
+                  >
+                    Edit Project
+                  </button>
+                </div>
+              ))}
+            </div>
+            <div className="p-2 text-center rounded-md bg-blue-900 mt-10 hover:bg-blue-600 hover:scale-95 transition-transform">
+              <button type="submit" className="text-white text-xl font-mono" onClick={() => navigate("/Vista-proyecto")}>
+                New Project
+              </button>
+            </div>
           </div>
-          <div className="p-2 text-center rounded-md bg-blue-900 mt-4">
-            <button type="submit" className="text-white text-xl font-mono" onClick={() => navigate("/Vista-proyecto")}>
-              New Project
-            </button>
-          </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </>
   );
 }
 
