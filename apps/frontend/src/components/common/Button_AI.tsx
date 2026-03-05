604a553490359f1fc653323135ee6fd4c9ead8cd
Carlos Enrique Contreras Duque <carlos.contredu@gmail.com>
2026-03-05 16:12:16 +0000

    Actualizada vista control-avance

---
+++ apps/frontend/src/components/common/Button_AI.tsx
@@ -0,0 +1,13 @@
+import React, { } from 'react';
+
+
+export const ButtonAI: React.FC = () => {
+
+    return (
+        <>
+            <div className="flex justify-end items-center pr-3 py-4 hover:scale-95 transition-transform duration-300">
+                <button type="button" id="AI" className="bg-gradient-to-r from-blue-900 to-orange-700 text-white py-2 px-4 rounded w-40 h-10 flex items-center justify-center">AI Analysis ✨</button>
+            </div>
+        </>
+    )
+}
--- apps/frontend/src/pages/Control-avance.tsx
+++ apps/frontend/src/pages/Control-avance.tsx
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import icons_trade_types from "../pages/Report/icons_trades_types";
+import { ButtonPlans } from '../components/common/Button_plans';
+import { ButtonDelete } from '../components/common/Button_delete';
+import { ButtonAI } from '../components/common/Button_AI';
 
 const ControlAvance: React.FC = () => {
 
@@ -17,14 +21,13 @@ const ControlAvance: React.FC = () => {
 
     const projectData = [
         { id: 1, name: "Project Alpha" },
-        // { id: 2, name: "Project Beta" },
-        // { id: 3, name: "Project Gamma" },
     ]
 
     const workers = [
         { id: 1, name: "Ramon Alcaide Gomez", role: "construction worker" },
         { id: 2, name: "Paco Fernandez Rodriguez", role: "Plumber" },
         { id: 3, name: "Piero Medina", role: "electrician" },
+        
     ]
 
     const projectManagers = [
@@ -34,15 +37,20 @@ const ControlAvance: React.FC = () => {
 
     const phases = [
         { id: 1, title: 'Start project', status: 'Started', state: 'completed' },
-        { id: 2, title: 'Planning project', status: 'In progress', state: 'pending' },
-        { id: 3, title: 'Execution of the project', status: 'Pending approval', state: 'none' },
+        { id: 2, title: 'Planning project', status: 'In progress', state: 'current' },
+        { id: 3, title: 'Execution of the project', status: 'Pending', state: 'none' },
         { id: 4, title: 'Project closing', status: 'Pending', state: 'none' },
     ]
 
+    const iconsStyles = {
+        "Plumbing": "bg-blue-200 border-blue-300",
+        "Electrical": "bg-green-200 border-green-300",
+    }
+
     const trades = [
-        { id: 1, name: "Carpentry" },
-        { id: 2, name: "Plumbing" },
-        { id: 3, name: "Electrical" },
+        { id: 1, name: "Carpintero", icon: icons_trade_types["Carpintero"] },
+        { id: 2, name: "Fontanero", icon: icons_trade_types["Fontanero"] },
+        { id: 3, name: "Electricista", icon: icons_trade_types["Electricista"] },
     ]
 
     const stateStyles = {
@@ -54,125 +62,129 @@ const ControlAvance: React.FC = () => {
 
     return (
         <>
-            {projectData.length === 0 ? (
-                <div className="container  bg-white mx-auto text-black text-center p-5 m-5">
-                    <h1 className="text-3xl font-bold mb-6">No se encontraron proyectos</h1>
-                </div>
-            ) : (projectData.map((project) => (
-                <div className=" flex flex-row mx-auto bg-white text-black border-b-4 border-gray-600 mt-5" key={project.id}>
-                    <div className="basis-2/3 bg-white text-black text-start flex items-center px-5" key={project.id}>
-                        <h1 className="text-3xl font-bold mb-3">{project.name}</h1>
+            <div className="v-screen bg-white">
+                {projectData.length === 0 ? (
+                    <div className="mx-auto text-black text-center p-5 m-5">
+                        <h1 className="text-3xl font-bold mb-6">There are no projects available.</h1>
                     </div>
-                    <div className="basis-1/3 flex justify-end items-center pr-3">
-                        <button className="bg-blue-900 text-white hover:bg-blue-600 py-2 px-4 rounded w-40 h-10 flex items-center justify-center">Planos</button>
-                    </div>
-                </div>
-            ))
-            )}
-
-            <div>
-                <div>
-                    {/* Project Manager */}
-                    <div className="bg-white text-black p-5">
-                        <h2 className="text-2xl font-semibold my-2">Project manager</h2>
-                        <div
-                            className="py-2 px-2 flex gap-4 items-center border border-gray-300 rounded-lg bg-sky-100"
-                        >
-                            <img
-                                src="#"
-                                alt="Foto"
-                                className="size-20 rounded-full border bg-gray-400 object-cover"
-                            />
-                            <h3 className="font-bold">{projectManagers[0]?.name}</h3>
-                            <p>{projectManagers[0]?.role}</p>
+                ) : (projectData.map((project) => (
+                    <div className=" flex text-black border-b-4 border-gray-600 pt-5" key={project.id}>
+                        <div className="basis-2/3 text-black text-start flex items-center px-5" key={project.id}>
+                            <h1 className="text-3xl font-bold mb-3">{project.name}</h1>
                         </div>
-                    </div >
-
-                    {/* <!-- equipo asignado --> */}
-                    <div className="bg-white text-black p-5">
-                        <h2 className="mt-4 text-2xl font-semibold my-2">Equipo asignado</h2>
-                        {workers.length === 0 ? (
-                            <p>There is not workers assigned to this project.</p>
-                        ) : (
-                            workers.map((worker) => (
-                                <div className="border border-gray-300 rounded-lg" key={worker.id}>
-                                    <div
-                                        className="py-2 px-2 flex gap-4 items-center border border-gray-300 rounded-lg bg-sky-100">
-                                        <img
-                                            src="#"
-                                            alt="Foto"
-                                            className="size-20 rounded-full border bg-gray-400 object-cover"
-                                        />
-                                        <h3 className="font-bold">{worker.name}</h3>
-                                        <p>{worker.role}</p>
-                                    </div>
-                                </div>
-                            ))
-                        )}
-                    </div>
-                </div>
-
-
-                <div>
-                    {/* <!-- phases of the project -->*/}
-                    <div className="relative pt-2 bg-white text-black p-5">
-                        <h2 className="text-xl font-semibold text-[#0A1F61] mb-8">Phases of the project</h2>
-
-                        <div className="mb-10">
-                            {phases.map((phase, index) => (
-                                <div key={phase.id} className="flex relative pb-4">
-                                    <div className="flex flex-col items-center justify-center mr-6 relative">
-                                        <div className={`w-6 h-6 rounded-full z-10 flex items-center justify-center ${stateStyles[phase.state]}`}>
-                                            {phase.state === 'completed' && <CheckIcon />}
-                                        </div>
-
-                                        {index < phases.length - 1 && (
-                                            <div className={`absolute top-6 bottom-[-50px] w-[2px] ${phase.state === 'completed' ? 'bg-green-500' : 'bg-gray-300'
-                                                }`}></div>
-                                        )}
-                                    </div>
-
-                                    <div className="flex-1 bg-[#E6EDF8] border border-[#B0C0D0] rounded-lg p-3">
-                                        <h3 className={`text-base font-medium m-0 mb-1 ${phase.state === 'pending' ? 'text-gray-500' : 'text-gray-900'
-                                            }`}>
-                                            {phase.title}
-                                        </h3>
-                                        <p className="text-sm text-gray-500 m-0">{phase.status}</p>
-                                    </div>
-                                </div>
-                            ))}
+                        <div className="flex basis-1/3 justify-end">
+                            <ButtonAI />
+                            <ButtonPlans />
+                            <ButtonDelete />
                         </div>
+                    </div>
+                ))
+                )}
 
-                        {/* <!-- Trades section --> */}
-                        <div className="bg-white text-black p-5">
-                            <h2 className="text-2xl font-semibold my-2">Trades</h2>
-                            {trades.length === 0 ? (
-                                <p>There is not trades assigned to this project.</p>
+                <div className="grid grid-cols-2 mx-auto pb-10">
+                    <div className="col-span-1">
+                        {/* Project Manager */}
+                        <div className="text-black p-5">
+                            <h2 className="text-3xl font-bold my-2">Project manager</h2>
+                            <div
+                                className="py-2 px-2 flex gap-4 items-center pt-10"
+                            >
+                                <p className="size-10 rounded-full border bg-orange-300 flex items-center justify-center"><UserIcon /></p>
+                                <div className="flex flex-col pl-4" >
+                                    <h3 className="font-bold text-2xl">{projectManagers[0]?.name}</h3>
+                                    <p>{projectManagers[0]?.role}</p>
+                                </div>
+                            </div>
+                        </div >
+
+                        {/* <!-- equipo asignado --> */}
+                        <div className="text-black p-5">
+                            <h2 className="mt-4 text-3xl font-bold my-2">Assigned team</h2>
+                            {workers.length === 0 ? (
+                                <p>There is not workers assigned to this project.</p>
                             ) : (
-                                trades.map((trade) => (
-                                    <div
-                                        className="py-1 px-2 my-3 flex w-40 border border-gray-300 rounded-lg bg-sky-100"
-                                        key={trade.id}
-                                    >
-                                        <p>{trade.name}</p>
+                                workers.map((worker) => (
+                                    <div className="" key={worker.id}>
+                                        <div
+                                            className="py-2 px-2 flex gap-4 items-center mb-3 pt-10">
+                                            <p className="size-10 rounded-full border bg-orange-300 flex items-center justify-center"><UserIcon /></p>
+                                            <div className="flex flex-col">
+                                                <h3 className="font-bold text-2xl">{worker.name}</h3>
+                                                <p>{worker.role}</p>
+                                            </div>
+                                        </div>
                                     </div>
                                 ))
                             )}
+                        </div>
+                    </div>
 
+
+                    <div className="col-span-1 p-2">
+                        {/* <!-- phases of the project -->*/}
+                        <div className="relative text-black p-5">
+                            <h2 className="text-xl font-semibold text-[#0A1F61] mb-8">Phases of the project</h2>
+
+                            <div className="mb-10">
+                                {phases.map((phase, index) => (
+                                    <div key={phase.id} className="flex relative pb-4">
+                                        <div className="flex flex-col items-center justify-center mr-6 relative">
+                                            <div className={`w-6 h-6 rounded-full z-10 flex items-center justify-center ${stateStyles[phase.state]}`}>
+                                                {phase.state === 'completed' && <CheckIcon />}
+                                            </div>
+
+                                            {index < phases.length - 1 && (
+                                                <div className={`absolute top-6 bottom-[-50px] w-[2px] ${phase.state === 'completed' ? 'bg-green-500' : 'bg-gray-300'
+                                                    }`}></div>
+                                            )}
+                                        </div>
+
+                                        <div className="flex-1 bg-[#E6EDF8] border border-[#B0C0D0] rounded-lg p-3">
+                                            <h3 className={`text-base font-medium m-0 mb-1 ${phase.state === 'pending' ? 'text-gray-500' : 'text-gray-900'
+                                                }`}>
+                                                {phase.title}
+                                            </h3>
+                                            <p className="text-sm text-gray-500 m-0">{phase.status}</p>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+
+                            {/* <!-- Trades section --> */}
+                            <div className="flex flex-wrap gap-3 text-black p-5">
+                                <div className="w-full">
+                                    <h2 className=" text-2xl font-semibold my-2">Trades</h2>
+                                </div>
+                                {trades.length === 0 ? (
+                                    <p>There is not trades assigned to this project.</p>
+                                ) : (
+                                    trades.map((trade) => {
+                                        const tradeInfo = icons_trade_types.find(
+                                            (item) => item.trade_type === trade.name
+                                        );
+
+                                        return (
+                                            <div
+                                                key={trade.id}
+                                                className={`flex items-center gap-2 px-4 py-2 rounded-lg text-white font-medium shadow-md
+                                            ${tradeInfo?.color_assigned || "bg-gray-400"}`}
+                                            >
+                                                {tradeInfo?.icon}
+                                                <span>{trade.name}</span>
+                                            </div>
+                                        );
+                                    })
+                                )}
+
+                            </div>
                         </div>
                     </div>
                 </div>
 
-                {/* <!-- Buttons --> */}
-                <div className="flex justify-around bg-white text-black p-5">
-                    {/* <!-- Button approve progress  --> */}
-                    <div className="p-2 my-3 text-center bg-blue-900 hover:bg-blue-600 mt-4 w-60 rounded">
-                        <button type="submit" id="new-project" className="text-white text-xl font-mono">Approve progress</button>
-                    </div>
-
-                    {/* button AI analysis */}
-                    <div className="p-2 my-3 text-center bg-blue-900 hover:bg-blue-600 mt-4 w-60 rounded bg-gradient-to-r from-blue-900 to-orange-700">
-                        <button type="submit" id="ai-analysis" className="text-white text-xl font-mono">AI Analysis ✨</button>
+                {/* <!-- Button approve progress  --> */}
+                <div className="p-7">
+                    <div className="p-2 my-3 text-center bg-blue-900 hover:bg-blue-600 hover:scale-95 transition-transform mt-4 rounded">
+                        <button type="submit" id="approve_progress" className="text-white text-xl font-mono">Approve progress</button>
                     </div>
                 </div>
             </div>
