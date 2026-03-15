db93d21fa8f458ace767ed6e8966b4d09114ecfc
Alex3glez <a5l6e7x8@gmail.com>
2026-03-15 02:26:36 +0000

    fixes authmiddleware.js

--- apps/backend/src/middleware/auth.middleware.js
+++ apps/backend/src/middleware/auth.middleware.js
@@ -2,14 +2,24 @@ import jwt from "jsonwebtoken";
 import config from "../config/index.js";
 
 export const protect = (req, res, next) => {
-  const token = req.cookies.token;
-  if (!token) return res.status(401).json({ message: "Not authorized" });
+  let token;
+
+  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+    token = req.headers.authorization.split(" ")[1];
+  } 
+  else if (req.cookies && req.cookies.token) {
+    token = req.cookies.token;
+  }
+
+  if (!token) {
+    return res.status(401).json({ message: "Not authorized, no token provided" });
+  }
 
   try {
     const decoded = jwt.verify(token, config.jwtSecret);
     req.user = decoded;
     next();
-  } catch {
-    res.status(401).json({ message: "Invalid token" });
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid token" });
   }
 };
